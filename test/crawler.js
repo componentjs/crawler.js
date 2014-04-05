7bd8d3e1c542730f88f8c2085a443ea1e2bfe585
Jonathan Ong <jonathanrichardong@gmail.com>
2014-04-05 15:02:09 -0700

    don't crawl repositories with issues disabled

--- test/crawler.js
+++ test/crawler.js
@@ -9,7 +9,7 @@ describe('crawler', function () {
   it('should delete any current .json', function (done) {
     store.client.deleteFile('/db.json', done);
     store.json = {
-      users: {},
+      users: [],
       components: [],
     };
   })
@@ -17,7 +17,7 @@ describe('crawler', function () {
   it('should crawl jonathanong', co(function* () {
     yield* crawler.patch('jonathanong');
     var json = store.json;
-    json.users.jonathanong.should.be.ok;
+    json.users.should.include('jonathanong');
     assert(json.components.some(function (component) {
       return component.name === 'horizontal-grid-packing';
     }))
